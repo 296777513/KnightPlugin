334db9989ecdf667b6dcfe43f95e2076b6a2e87c
liyachao <liyachao.knight@bytedance.com>
2019-01-21 11:38:41 +0800

    feature:create subActivity

---
+++ app/src/main/java/com/knight/plugin/SubActivity.kt
@@ -0,0 +1,11 @@
+package com.knight.plugin
+
+import android.app.Activity
+import android.os.Bundle
+
+class SubActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

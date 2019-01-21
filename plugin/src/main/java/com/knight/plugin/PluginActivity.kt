8cf3ece95d9716c68e8365aeb3d71dde62131f50
liyachao <liyachao.knight@bytedance.com>
2019-01-21 13:48:11 +0800

    feature:finish getResources

--- plugin/src/main/java/com/knight/plugin/PluginActivity.kt
+++ plugin/src/main/java/com/knight/plugin/PluginActivity.kt
@@ -1,12 +1,19 @@
 package com.knight.plugin
 
+import android.app.Activity
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class PluginActivity : AppCompatActivity() {
+class PluginActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plugin)
+    }
+
+
+    override fun getResources(): Resources {
+        return if (application != null && application.resources != null) application.resources else super.getResources()
     }
 }
